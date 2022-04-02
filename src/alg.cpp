e3b690577be15cb1bb6e8afaf130d4996dd03a64
Anna Shevelyova <99125817+shvlvnn@users.noreply.github.com>
2022-04-02 22:42:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,82 +1,101 @@
+// Copyright 2021 NNTU-CS
 #include <string>
 #include <map>
 #include "tstack.h"
-using namespace std;
-int prior(char ch) {
-    switch (ch) {
-        case '(': return 0;
-        case ')': return 1;
-        case '+': return 2;
-        case '-': return 2;
-        case '*': return 3;
-        case '/': return 3;
-        default: return -1;
-    }
+int prior(char op) {
+  switch (op) {
+    case '(':
+      return 0;
+    case ')':
+      return 1;
+    case '+':
+      return 2;
+    case '-':
+      return 2;
+    case '/':
+      return 3;
+    case '*':
+      return 3;
+    case ' ':
+      return 5;
+    default:
+      return 4;
+  }
 }
-string infx2pstfx(string inf) {
-    TStack<char> stack1;
-    string tmp = "";
-    for (int i = 0; i < inf.size(); i++) {
-        char ch = inf[i];
-        int k = prior(ch);
-        if (k == -1)
-        tmp.append(string(1, ch));
-        else
-        if (stack1.isEmpty() || k == 0 || k > prior(stack1.get()))
-            stack1.push(ch);
-        else {
-            if (ch == ')')
-            while (true) {
-                char sym = stack1.get();
-                stack1.pop();
-            if (sym != '(')
-            tmp.append(string(1, sym));
-            else
-            break;
-            }
-            else {
-                while (!stack1.isEmpty()) {
-                    char lastStackEl = stack1.get();
-                    stack1.pop();
-                    if (prior(lastStackEl) >= k)
-                    tmp.append(string(1, lastStackEl));
-                }
-                stack1.push(ch);
-            }
+int calculate(char op, int a, int b) {
+  switch (op) {
+      case '+':
+        return b + a;
+      case '-':
+        return b - a;
+      case '*':
+        return b * a;
+      case '/':
+        if (a != 0)
+          return b / a;
+      default:
+        return 0;
+  }
+}
+
+std::string infx2pstfx(std::string inf) {
+  std::string post;
+  char prob = ' ';
+  TStack <char, 100> t;
+  for (int i = 0; i < inf.size(); i++) {
+    if (prior(inf[i]) == 4) {
+      post.push_back(inf[i]);
+      post.push_back(prob);
+    } else {
+      if (prior(inf[i]) == 0) {
+        t.push(inf[i]);
+      } else if (t.isEmpty()) {
+        t.push(inf[i]);
+      } else if ((prior(inf[i]) > prior(t.get()))) {
+        t.push(inf[i]);
+      } else if (prior(inf[i]) == 1) {
+        while (prior(t.get()) != 0) {
+          post.push_back(t.get());
+          post.push_back(prob);
+          t.pop();
         }
-    }
-    while (!stack1.isEmpty()) {
-        char lastStackEl = stack1.get();
-        stack1.pop();
-        tmp.append(string(1, lastStackEl));
-    }
-    return tmp;
-}
-int excute_calc(int k1, int k2, char pst) {
-    switch (pst)
-    {
-    case '+': return k1 + k2;
-    case '-': return k1 - k2;
-    case '*': return k1 * k2;
-    case '/': return k1 / k2;
-    default: return -1;
-    }
-}
-int eval(string pst) {
-    TStack<int> stack2;
-    for (int i = 0; i < pst.size(); i++) {
-        char ch = pst[i];
-        int priority = prior(ch);
-        if (priority == -1)
-        stack2.push(ch - 48);
-        else {
-            int k1 = stack2.get();
-            stack2.pop();
-            int k2 = stack2.get();
-            stack2.pop();
-            int res = excute_calc(k2, k1, ch);
-            stack2.push(res);
+        t.pop();
+      } else {
+        while (!t.isEmpty() && (prior(inf[i]) <= prior(t.get()))) {
+          post.push_back(t.get());
+          post.push_back(prob);
+          t.pop();
         }
+        t.push(inf[i]);
+      }
     }
-    return stack2.get();
+  }
+  while (!t.isEmpty()) {
+    post.push_back(t.get());
+    post.push_back(prob);
+    t.pop();
+  }
+  for (int j = 0; j < post.size(); j++) {
+    if (post[post.size()-1] == ' ')
+      post.erase(post.size()-1);
+  }
+  return post;
+}
+
+int eval(std::string pref) {
+  TStack <int, 100> t;
+  int res = 0;
+  for (int i = 0; i < pref.size(); i++) {
+    if (prior(pref[i]) == 4) {
+      t.push(pref[i] - '0');
+    } else if (prior(pref[i]) < 4) {
+      int x = t.get();
+      t.pop();
+      int y = t.get();
+      t.pop();
+      t.push(calculate(pref[i], x, y));
+    }
+  }
+  res = t.get();
+  return res;
 }
